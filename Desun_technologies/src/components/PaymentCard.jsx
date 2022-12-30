f7f1bf7ebb5aa785cae7e705b5306173303b2ebc
Bhavesh Chavhan <97518972+Bhavesh299@users.noreply.github.com>
2022-12-30 18:13:21 +0530

    Update And Done

--- Desun_technologies/src/components/PaymentCard.jsx
+++ Desun_technologies/src/components/PaymentCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import styled from "../styles/paymentCard.module.css";
+import { useNavigate } from "react-router-dom";
+
 
 const PaymentCard = () => {
 
+    const navigate = useNavigate();
+
     const Handelchange = () => {
-        
+        alert("Booking Successfully Complete")
+        navigate("/GameList");
+
     }
    
   return (
@@ -13,9 +19,9 @@ const PaymentCard = () => {
         <div className={styled.mainDiv}>
             <input type="Number" placeholder='Enter Card Number' required />
             <input type="text" placeholder='Enter Card Holder Name' required/>
-            <input type="Number" placeholder='Expiry Date' required/>
+            <input type="text" placeholder='Expiry Date' required/>
             <input type="password" placeholder='Enter 3 Digit CVV' required />
-            <button onClick={()=> Handelchange} className={styled.btn}>Pay</button>
+            <button onClick={()=> Handelchange()} className={styled.btn}>Pay</button>
 
             
         </div>
